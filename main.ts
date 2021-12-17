380cccc471aec88f80874a81561d4b7d9741a550
thejoshuahendrix <56802866+thejoshuahendrix@users.noreply.github.com>
2021-12-17 18:42:38 -0500

    added comments

--- main.ts
+++ main.ts
@@ -1,51 +1,40 @@
-const ErrorParse = (err: any, counter?: number, character?: String, prefix?: String) => {
-    let trimmedError = prefix || "";
-    if (counter) {
-        let count = 0;
-        for (let i = 0; i < err.stack.length; i++) {
-            trimmedError += err.stack[i];
-            if (character) {
-                if (err.stack[i] == character) {
-                    count++
-                    if (count >= counter) {
-                        break;
-                    }
-                }
-            } else {
-                if (err.stack[i] == "\n") {
-                    count++
-                    if (count >= counter) {
-                        break;
-                    }
-                }
-            }
-        }
-    } else {
-        for (let i = 0; i < err.stack.length; i++) {
-            trimmedError += err.stack[i];
-            if (character) {
-                if (err.stack[i] == character) {
-                    break;
-                }
-            } else {
-                if (err.stack[i] == "\n") {
-                    break;
-                }
-            }
 
+/**
+* An Error Parser to handle parsing,
+* It counts the character and returns as many lines as you input for the counter. 
+* Character is line breaks by default.
+*/
+const ErrorParse = (err: any, counter = 1, character = "\n") => {
+
+    let trimmedError = "";
+    let count = 0;
+    for (let i = 0; i < err.stack.length; i++) {
+        trimmedError += err.stack[i];
+        if (err.stack[i] == character && counter) {
+            count++
+            if (count >= counter) {
+                break;
+            }
         }
     }
     return trimmedError;
 }
 
-const ErrorHandler = (func: () => void, counter?: number, character?: String, prefix?: String) => {
+/**
+* An Error Handler to customize your console.logging,
+* counter is how many lines you want, character you can customize the the character you parse by. 
+* It is line breaks by default.
+*/
+const ErrorHandler = (func: () => void, counter?: number, character?: string) => {
     try {
         func()
     } catch (err: any) {
-        console.log(ErrorParse(err, counter, character, prefix))
+        console.log(ErrorParse(err, counter, character))
     }
 }
 
+
+//Error Handler Test
 let answer = false;
 
 const answerTest = (ans: boolean) => {
@@ -57,4 +46,4 @@ const answerTest = (ans: boolean) => {
     };
 };
 
-ErrorHandler(() => answerTest(answer), 10)
+ErrorHandler(() => answerTest(answer), 2)
