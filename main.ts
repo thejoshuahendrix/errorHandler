fc1255e54fd861bab529e9e448e4b571dcef6dc6
thejoshuahendrix <56802866+thejoshuahendrix@users.noreply.github.com>
2021-12-17 21:27:57 -0500

    compiled, ready for npm

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
-
 /**
 * An Error Parser to handle parsing,
 * It counts the character and returns as many lines as you input for the counter. 
-* Character is line breaks by default.
+* @param counter is how many lines in the console you allow.
+* @param character is what to count for the counter line breaks by default.
 */
-const ErrorParse = (err: any, counter = 1, character = "\n") => {
+export const ErrorParse = (err: any, counter = 1, character = "\n") => {
 
     let trimmedError = "";
     let count = 0;
@@ -22,14 +22,15 @@ const ErrorParse = (err: any, counter = 1, character = "\n") => {
 
 /**
 * An Error Handler to customize your console.logging,
-* counter is how many lines you want, character you can customize the the character you parse by. 
-* It is line breaks by default.
+* @param counter is how many lines in the console you allow, one by default.
+* @param character the character to count for parsing, line breaks by default.
 */
-const ErrorHandler = (func: () => void, counter?: number, character?: string) => {
+export const ErrorHandler = (func: () => any, counter?: number, character?: string) => {
     try {
-        func()
+        return func();
     } catch (err: any) {
         console.log(ErrorParse(err, counter, character))
+        return null;
     }
 }
 
@@ -40,10 +41,14 @@ let answer = false;
 const answerTest = (ans: boolean) => {
     if (ans === true) {
         console.log('Here we are')
+        return "Hello";
     }
     else {
         throw new Error("Answer has to be true")
     };
 };
 
-ErrorHandler(() => answerTest(answer), 2)
+let returnerror = ErrorHandler(() => answerTest(answer), 2)
+console.log(returnerror);
+
+
